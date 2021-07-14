7b18c049ec339fc052f5115bdcecdb639f2eb8d7
Yaroslav Tumanov <airhole.ua@gmail.com>
2021-07-14 14:58:13 +0300

    fix interfaces

--- contracts/DGVC.sol
+++ contracts/DGVC.sol
@@ -2,8 +2,8 @@ pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/IDGVC.sol";
-import "./libraries/SafeERC20.sol";
 
 
 contract DGVC is IDGVC, Context, Ownable {
--- contracts/interfaces/IDGVC.sol
+++ contracts/interfaces/IDGVC.sol
@@ -1,5 +1,5 @@
 pragma solidity 0.8.4;
-import "./IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IDGVC is IERC20 {
     function burn(uint amount) external returns (bool);
